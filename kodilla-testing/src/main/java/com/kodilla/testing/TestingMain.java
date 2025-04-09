d4ea82eb6c3b8476c2dc462104b429500041b4fc
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-04-09 22:18:07 +0200

    6.2 first unit tests - user and calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        // SimpleUser
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("test FAILED!");
+        }
+
+        // Calculator
+        Calculator calculator = new Calculator();
+        int resultOfAddition = calculator.add(10, 20);
+        int resultOfSubtraction = calculator.subtract(10, 20);
+
+        if (resultOfAddition == 30) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("test FAILED!");
+        }
+
+        if (resultOfSubtraction == -10) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("test FAILED!");
+        }
     }
 }
