194333d5cd232ca3ff601ad6966c3cf22d599c60
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-04-20 22:56:08 +0200

    7.1 stream main - references to methods

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -4,19 +4,22 @@ import com.kodilla.stream.lambda.ExecuteSaySomething;
 import com.kodilla.stream.lambda.Executor;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {
-        Processor processor = new Processor();
-//        Executor codeToExecute = () -> System.out.println("This is an example text.");
-//        processor.execute(codeToExecute);
-        // alternative without variable
-        processor.execute(() -> System.out.println("This is an example text."));
-
         ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambda");
         expressionExecutor.executeExpression(5,3,(a,b) -> a + b);
         expressionExecutor.executeExpression(5,3,(a,b) -> a - b);
         expressionExecutor.executeExpression(5,3,(a,b) -> a * b);
         expressionExecutor.executeExpression(5,3,(a,b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(5,3, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(5,3, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(5,3, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(5,3, FunctionalCalculator::divideAByB);
     }
 }
