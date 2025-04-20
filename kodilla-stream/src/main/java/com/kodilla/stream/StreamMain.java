ce1263a438399ecf41cc2faaec598020026d27af
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-04-20 22:00:58 +0200

    7.1 stream main - lambda expression

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,12 +1,15 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.lambda.ExecuteSaySomething;
+import com.kodilla.stream.lambda.Executor;
 import com.kodilla.stream.lambda.Processor;
 
 public class StreamMain {
     public static void main(String[] args) {
         Processor processor = new Processor();
-        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
-        processor.execute(executeSaySomething);
+//        Executor codeToExecute = () -> System.out.println("This is an example text.");
+//        processor.execute(codeToExecute);
+        // alternative without variable
+        processor.execute(() -> System.out.println("This is an example text."));
     }
 }
