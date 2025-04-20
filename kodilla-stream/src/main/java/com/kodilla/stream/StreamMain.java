d981797644575ebcd3ce60fbea4d894cdd84dd0c
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-04-20 21:52:30 +0200

    7.1 stream main second implementation with processor and executor

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,12 +1,12 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.SaySomething;
+import com.kodilla.stream.lambda.ExecuteSaySomething;
+import com.kodilla.stream.lambda.Processor;
 
 public class StreamMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream");
-
-        SaySomething saySomething = new SaySomething();
-        saySomething.say();
+        Processor processor = new Processor();
+        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
+        processor.execute(executeSaySomething);
     }
 }
