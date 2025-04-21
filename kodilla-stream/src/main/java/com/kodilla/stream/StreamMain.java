0b958ae132cc9ee35104b46a9e7f86cbbe4d476e
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-04-21 22:14:38 +0200

    7.3 Stream - map

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -6,22 +6,19 @@ import com.kodilla.stream.lambda.ExecuteSaySomething;
 import com.kodilla.stream.lambda.Executor;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.person.People;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
 
 public class StreamMain {
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        String poem = "Whispers drift through silent trees.";
-
-        poemBeautifier.executeBeautifier(poem, p -> p.toUpperCase());
-        poemBeautifier.executeBeautifier(poem, p -> p = ">>> " + p + " <<<");
-        poemBeautifier.executeBeautifier(poem, p -> p = "\u001B[31m" + p + "\u001B[0m");
-        poemBeautifier.executeBeautifier(poem, p -> String.join("\n", p.split(" ")));
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        People.getList().stream()
+//                .map(s -> s.toUpperCase())
+                .map(String::toUpperCase)
+//                .forEach(System.out::println);
+                .forEach(s -> System.out.println(s));
     }
 }
