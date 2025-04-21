b2110ce656388ae898d6d18f3d9690b77f0704ab
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-04-21 22:21:59 +0200

    7.3 Stream - cascading operations

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -16,7 +16,10 @@ import java.util.List;
 public class StreamMain {
     public static void main(String[] args) {
         People.getList().stream()
+                .map(String::toUpperCase)
                 .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(" ") +2) + ".")
+                .filter(s -> s.substring(0, 1).equals("M"))
                 .forEach(System.out::println);
     }
 }
