a12c1344bc8762c4f4b2179809f001f8ad1a7baf
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-04-21 22:31:15 +0200

    7.3 Stream - BookDirectory operations example

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,25 +1,14 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.ExecuteSaySomething;
-import com.kodilla.stream.lambda.Executor;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.lambda.Processor;
-import com.kodilla.stream.person.People;
-import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.book.BookDirectory;
 
-import javax.swing.*;
-import java.awt.*;
 import java.util.List;
 
 public class StreamMain {
     public static void main(String[] args) {
-        People.getList().stream()
-                .map(String::toUpperCase)
-                .filter(s -> s.length() > 11)
-                .map(s -> s.substring(0, s.indexOf(" ") +2) + ".")
-                .filter(s -> s.substring(0, 1).equals("M"))
+        BookDirectory theBookDirectory = new BookDirectory();
+        theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
                 .forEach(System.out::println);
     }
 }
