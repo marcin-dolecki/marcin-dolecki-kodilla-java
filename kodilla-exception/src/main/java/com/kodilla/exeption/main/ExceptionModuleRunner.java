9b6e7356a0560086f064c759a8d450509a8a7104
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-04-29 20:31:09 +0200

    8.3 Throws

--- kodilla-exception/src/main/java/com/kodilla/exeption/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exeption/main/ExceptionModuleRunner.java
@@ -1,10 +1,11 @@
 package com.kodilla.exeption.main;
 
 import com.kodilla.exeption.io.FileReader;
+import com.kodilla.exeption.io.FileReaderWithoutHandling;
 
 public class ExceptionModuleRunner {
     public static void main(String[] args) {
-        FileReader fileReader = new FileReader();
+        FileReaderWithoutHandling fileReader = new FileReaderWithoutHandling();
         fileReader.readFile();
     }
 }
