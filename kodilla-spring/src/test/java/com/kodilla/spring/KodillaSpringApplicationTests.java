ca62b6b40da343e493a9e95756721eef8bb0db8c
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-06-05 23:03:05 +0200

    configuration and bean declaration

--- kodilla-spring/src/test/java/com/kodilla/spring/KodillaSpringApplicationTests.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/KodillaSpringApplicationTests.java
@@ -2,6 +2,7 @@ package com.kodilla.spring;
 
 import com.kodilla.spring.shape.Circle;
 import com.kodilla.spring.shape.Shape;
+import com.kodilla.spring.shape.Square;
 import com.kodilla.spring.shape.Triangle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -31,6 +32,16 @@ class KodillaSpringApplicationTests {
 
 		assertEquals("This is a triangle", name);
 	}
+
+		@Test
+		void testSquareLoadedIntoContainer() {
+			ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+			Shape shape = context.getBean(Square.class);
+
+			String name = shape.getShapeName();
+
+			assertEquals("This is a square", name);
+		}
 
 	@Test
 	void contextLoads() {
