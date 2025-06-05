518dffbc0fce48812c3c8eff033d88dcd77dd082
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-06-05 16:31:07 +0200

    used dependency injection via constructor

--- kodilla-spring-intro/src/test/java/com/kodilla/spring/intro/shape/DrawerTestSuite.java
+++ kodilla-spring-intro/src/test/java/com/kodilla/spring/intro/shape/DrawerTestSuite.java
@@ -2,10 +2,26 @@ package com.kodilla.spring.intro.shape;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class DrawerTestSuite {
     @Test
-    void testDoDrawing() {
-        Drawer drawer = new Drawer();
-        drawer.doDrawing();
+    void testDoDrawingWithCircle() {
+        Circle circle = new Circle();
+
+        Drawer drawer = new Drawer(circle);
+        String result = drawer.doDrawing();
+
+        assertEquals("This is a circle", result);
+    }
+
+    @Test
+    void testDoDrawingWithTriangle() {
+        Triangle triangle = new Triangle();
+
+        Drawer drawer = new Drawer(triangle);
+        String result = drawer.doDrawing();
+
+        assertEquals("This is a triangle", result);
     }
 }
