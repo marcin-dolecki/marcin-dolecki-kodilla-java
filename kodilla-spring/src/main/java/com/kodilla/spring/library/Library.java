88066321ca1231dc44efd74c24d878ae551b6365
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-06-06 21:02:26 +0200

    injection by constructor with autowired for more than one constructor

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -1,10 +1,12 @@
 package com.kodilla.spring.library;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/* INJECTION BY CONSTRUCTOR
 @Service
 public final class Library {
     private final List<String> books = new ArrayList<>();
@@ -12,6 +14,30 @@ public final class Library {
 
     public Library(final LibraryDbController libraryDbController) {
         this.libraryDbController = libraryDbController;
+    }
+
+    public void saveToDb() {
+        libraryDbController.saveData();
+    }
+
+    public void loadFromDb() {
+        libraryDbController.loadData();
+    }
+}
+*/
+
+@Service
+public final class Library {
+    private final List<String> books = new ArrayList<>();
+    private LibraryDbController libraryDbController;
+
+    @Autowired
+    public Library(final LibraryDbController libraryDbController) {
+        this.libraryDbController = libraryDbController;
+    }
+
+    public Library() {
+        // do nothing
     }
 
     public void saveToDb() {
