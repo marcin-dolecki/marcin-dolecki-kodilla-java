35e170b2481ad0e05090e6227b1b2f34fc8d148e
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-06-06 20:56:23 +0200

    injection by constructor

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.spring.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+public class LibraryTestSuite {
+    @Test
+    void testLoadFromDb() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.library");
+        Library library = context.getBean(Library.class);
+
+        library.loadFromDb();
+    }
+
+    @Test
+    void testSaveToDb() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.library");
+        Library library = context.getBean(Library.class);
+
+        library.saveToDb();
+    }
+}
