733bd2a32ae0151760cd9a3dcb54a493fd626ca6
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-06-06 21:47:57 +0200

    used autowired in tests

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -1,12 +1,14 @@
 package com.kodilla.spring.library;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+/* USING GET BEAN
 @SpringBootTest
-public class LibraryTestSuite {
+class LibraryTestSuite {
     @Test
     void testLoadFromDb() {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.library");
@@ -20,6 +22,24 @@ public class LibraryTestSuite {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.library");
         Library library = context.getBean(Library.class);
 
+        library.saveToDb();
+    }
+}
+*/
+
+/* USING AUTOWIRED */
+@SpringBootTest
+class LibraryTestSuite {
+    @Autowired
+    private Library library;
+
+    @Test
+    void testLoadFromDb() {
+        library.loadFromDb();
+    }
+
+    @Test
+    void testSaveToDb() {
         library.saveToDb();
     }
 }
