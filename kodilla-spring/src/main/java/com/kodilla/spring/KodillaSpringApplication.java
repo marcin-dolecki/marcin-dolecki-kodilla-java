b156b772cd138c4cb0dfa0474adef621e52cf057
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-06-07 22:26:19 +0200

    spring boot application adnotation in main class

--- kodilla-spring/src/main/java/com/kodilla/spring/KodillaSpringApplication.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/KodillaSpringApplication.java
@@ -1,13 +1,18 @@
 package com.kodilla.spring;
 
+import com.kodilla.spring.library.Library;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class KodillaSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KodillaSpringApplication.class, args);
-	}
+		ApplicationContext context = SpringApplication.run(KodillaSpringApplication.class, args);
 
+		Library library = context.getBean(Library.class);
+		library.loadFromDb();
+	}
 }
