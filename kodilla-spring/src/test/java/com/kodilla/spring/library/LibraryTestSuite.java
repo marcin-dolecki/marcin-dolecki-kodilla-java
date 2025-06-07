2c4908e313532a303c28ad3f7aa0858f79f7a5bb
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-06-07 22:50:04 +0200

    application context

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 /* USING GET BEAN */
 //@SpringBootTest
 //class LibraryTestSuite {
@@ -28,18 +30,58 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 
 /* USING AUTOWIRED */
+//@SpringBootTest
+//class LibraryTestSuite {
+//    @Autowired
+//    private Library library;
+//
+//    @Test
+//    void testLoadFromDb() {
+//        library.loadFromDb();
+//    }
+//
+//    @Test
+//    void testSaveToDb() {
+//        library.saveToDb();
+//    }
+//}
+
+/* APPLICATION CONTEXT */
 @SpringBootTest
 class LibraryTestSuite {
-    @Autowired
-    private Library library;
-
     @Test
     void testLoadFromDb() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.library");
+        Library library = context.getBean(Library.class);
+
         library.loadFromDb();
     }
 
     @Test
     void testSaveToDb() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.library");
+        Library library = context.getBean(Library.class);
+
         library.saveToDb();
+    }
+
+    @Test
+    void testContext() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(LibraryConfig.class);
+
+        System.out.println("===== Beans list: ==== >>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<< ==== Beans list: =====");
+    }
+
+    @Test
+    void testContextFromWholeSpringPackage() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+
+        System.out.println("===== Beans list: ==== >>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<< ==== Beans list: =====");
     }
 }
