5f79e612e5a4376a65629681e95f6904b55f37ab
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-06-08 23:27:53 +0200

    added prices directly to Enums

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/enums/hotel/HotelApp.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/enums/hotel/HotelApp.java
@@ -1,5 +1,6 @@
 package com.kodilla.good.patterns.enums.hotel;
 
+/* OLD WAY --> with PriceList class
 public class HotelApp {
     public static void main(String[] args) {
         System.out.println("Accomodation prices:");
@@ -11,6 +12,25 @@ public class HotelApp {
         System.out.println("    - low season: " + PriceList.getDoubleBedPrice(Season.LOW));
         System.out.println("    - high season: " + PriceList.getDoubleBedPrice(Season.HIGH));
         System.out.println("    - holiday season: " + PriceList.getDoubleBedPrice(Season.HOLIDAY));
+
+        Season season = Season.HOLIDAY;
+        System.out.println(season);
+    }
+}
+ */
+
+/*NEW WAY --> prices directly from ENUM */
+public class HotelApp {
+    public static void main(String[] args) {
+        System.out.println("Accomodation prices:");
+        System.out.println("1. Single Bed rooms");
+        System.out.println("    - low season: " + Season.LOW.getSingleRoomPrice());
+        System.out.println("    - high season: " + Season.HIGH.getSingleRoomPrice());
+        System.out.println("    - holiday season: " + Season.HOLIDAY.getSingleRoomPrice());
+        System.out.println("2. Double Bed rooms");
+        System.out.println("    - low season: " + Season.LOW.getDoubleBedPrice());
+        System.out.println("    - high season: " + Season.HIGH.getDoubleBedPrice());
+        System.out.println("    - holiday season: " + Season.HOLIDAY.getDoubleBedPrice());
 
         Season season = Season.HOLIDAY;
         System.out.println(season);
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/enums/hotel/Season.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/enums/hotel/Season.java
@@ -1,7 +1,25 @@
 package com.kodilla.good.patterns.enums.hotel;
 
+import java.math.BigDecimal;
+
 public enum Season {
-    LOW,
-    HIGH,
-    HOLIDAY
+    LOW(new BigDecimal(250), new BigDecimal(350)),
+    HIGH(new BigDecimal(320), new BigDecimal(400)),
+    HOLIDAY(new BigDecimal(400), new BigDecimal(500));
+
+    private BigDecimal singleRoomPrice;
+    private BigDecimal doubleBedPrice;
+
+    Season(BigDecimal singleRoomPrice, BigDecimal doubleBedPrice) {
+        this.singleRoomPrice = singleRoomPrice;
+        this.doubleBedPrice = doubleBedPrice;
+    }
+
+    public BigDecimal getSingleRoomPrice() {
+        return singleRoomPrice;
+    }
+
+    public BigDecimal getDoubleBedPrice() {
+        return doubleBedPrice;
+    }
 }
