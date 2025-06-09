7c57b1d99b9df131cc921cdd9f7728db61048398
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-06-09 22:08:57 +0200

    without singleton

---
+++ kodilla-good-patterns/src/test/java/com/kodilla/good/patterns/singleton/SettingsFileEngineTestSuite.java
@@ -0,0 +1,42 @@
+package com.kodilla.good.patterns.singleton;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SettingsFileEngineTestSuite {
+    private static SettingsFileEngine settingsFileEngine;
+
+    @BeforeAll
+    public static void openSettingsFile() {
+        settingsFileEngine = new SettingsFileEngine();
+        settingsFileEngine.open("myapp.settings");
+    }
+
+    @AfterAll
+    public static void closeSettingsFile() {
+        settingsFileEngine.close();
+    }
+
+    @Test
+    void testGetFileName() {
+        String fileName = settingsFileEngine.getFileName();
+        System.out.println("Opened: " + fileName);
+        assertEquals("myapp.settings", fileName);
+    }
+
+    @Test
+    void testLoadSettings() {
+        boolean result = settingsFileEngine.loadSettings();
+        assertTrue(result);
+    }
+
+    @Test
+    void testSaveSettings() {
+        boolean result = settingsFileEngine.saveSettings();
+        assertTrue(result);
+    }
+}
