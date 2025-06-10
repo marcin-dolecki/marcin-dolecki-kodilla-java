261283652840239530a13997cbc30a810f6cf983
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-06-10 21:29:50 +0200

    example without prototype in progress

--- kodilla-good-patterns/src/test/java/com/kodilla/good/patterns/prototype/BoardTestSuite.java
+++ kodilla-good-patterns/src/test/java/com/kodilla/good/patterns/prototype/BoardTestSuite.java
@@ -1,4 +1,34 @@
 package com.kodilla.good.patterns.prototype;
 
-public class BoardTestSuite {
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BoardTestSuite {
+    @Test
+    void testToString() {
+        TaskList listToDo = new TaskList("To Do Tasks");
+        IntStream.iterate(1, n -> n + 1)
+                .limit(10)
+                .forEach(n -> listToDo.getTasks().add(new Task("To Do Task Number " + n)));
+
+        TaskList listInProgress = new TaskList("In Progress Tasks");
+        IntStream.iterate(1, n -> n + 1)
+                .limit(10)
+                .forEach(n -> listInProgress.getTasks().add(new Task("In Progress Task Number " + n)));
+
+        TaskList listDone = new TaskList("Done Tasks");
+        IntStream.iterate(1, n -> n + 1)
+                .limit(10)
+                .forEach(n -> listDone.getTasks().add(new Task("Done Task Number " + n)));
+
+        Board board  = new Board("Project number 1");
+        board.getLists().add(listToDo);
+        board.getLists().add(listInProgress);
+        board.getLists().add(listDone);
+        System.out.println(board);
+
+        assertEquals(3, board.getLists().size());
+    }
 }
