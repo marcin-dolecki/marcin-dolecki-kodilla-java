db7cb3e2bb05050cba97f45efb9440fce01a7c86
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-06-13 21:42:52 +0200

    Factory pattern completed

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/factory/Shape.java
@@ -0,0 +1,7 @@
+package com.kodilla.good.patterns.factory;
+
+public interface Shape {
+    String getName();
+    double getArea();
+    double getCircumference();
+}
---
+++ kodilla-good-patterns/src/test/java/com/kodilla/good/patterns/factory/ShapeFactoryTestSuite.java
@@ -0,0 +1,34 @@
+package com.kodilla.good.patterns.factory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ShapeFactoryTestSuite {
+    @Test
+    void testFactoryCircle() {
+        ShapeFactory factory = new ShapeFactory();
+        Shape circle = factory.makeShape(ShapeFactory.CIRCLE);
+
+        assertEquals(Math.PI * Math.pow(4.50, 2.0), circle.getArea(), 0);
+        assertEquals("The rounded circle", circle.getName());
+    }
+
+    @Test
+    void testFactorySquare() {
+        ShapeFactory factory = new ShapeFactory();
+        Shape square = factory.makeShape(ShapeFactory.SQUARE);
+
+        assertEquals(28.0, square.getCircumference(), 0);
+        assertEquals("The angular square", square.getName());
+    }
+
+    @Test
+    void testFactoryRectangle() {
+        ShapeFactory factory = new ShapeFactory();
+        Shape rectangle = factory.makeShape(ShapeFactory.RECTANGLE);
+
+        assertEquals(37.50, rectangle.getArea(), 0);
+        assertEquals("The long rectangle", rectangle.getName());
+    }
+}
