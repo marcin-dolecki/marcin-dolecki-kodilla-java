d143b76d45838aff59255a575a314afadd3d24e6
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-06-21 01:01:49 +0200

    communication with the data base

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -2,8 +2,11 @@ package com.kodilla.jdbc;
 
 import org.junit.jupiter.api.Test;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class DbManagerTestSuite {
@@ -12,5 +15,53 @@ class DbManagerTestSuite {
         DbManager dbManager = DbManager.getInstance();
 
         assertNotNull(dbManager.getConnection());
+    }
+
+    @Test
+    void testSelectUser() throws SQLException {
+        DbManager dbManager = DbManager.getInstance();
+
+        String sqlQuery = "select * from USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+        int counter = 0;
+        while (resultSet.next()) {
+            System.out.println(
+                    resultSet.getInt("ID") + ", " +
+                    resultSet.getString("FIRSTNAME") + ", " +
+                    resultSet.getString("LASTNAME")
+            );
+            counter++;
+        }
+        resultSet.close();
+        statement.close();
+        assertEquals(5, counter);
+    }
+
+    @Test
+    void testSelectUsersAndTasks() throws SQLException {
+        DbManager dbManager = DbManager.getInstance();
+
+        String sqlQuery = """
+            select u.FIRSTNAME, u.LASTNAME, i.SUMMARY
+            from USERS u
+            join ISSUES i on u.ID = i.USER_ID_ASSIGNEDTO
+        """;
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+        int counter = 0;
+        while (resultSet.next()) {
+            System.out.println(
+                    resultSet.getString("FIRSTNAME") + ", " +
+                    resultSet.getString("LASTNAME") + ", " +
+                    resultSet.getString("SUMMARY")
+            );
+            counter++;
+        }
+        resultSet.close();
+        statement.close();
+        assertEquals(12, counter);
     }
 }
