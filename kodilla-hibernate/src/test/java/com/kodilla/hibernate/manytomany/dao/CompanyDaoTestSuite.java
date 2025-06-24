ed68e5ea4e5d9b76466736356ac7abd3f6620352
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-06-24 23:51:52 +0200

    many to many relation completed

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -1,7 +1,13 @@
 package com.kodilla.hibernate.manytomany.dao;
 
+import com.kodilla.hibernate.manytomany.Company;
+import com.kodilla.hibernate.manytomany.Employee;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
 class CompanyDaoTestSuite {
@@ -9,5 +15,50 @@ class CompanyDaoTestSuite {
     @Autowired
     private CompanyDao companyDao;
 
-    
+    @Test
+    void testSaveManyToMany() {
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine ");
+        Company dataMaesters = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        dataMaesters.getEmployees().add(stephanieClarckson);
+        dataMaesters.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(johnSmith);
+        greyMatter.getEmployees().add(lindaKovalsky);
+
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+
+        companyDao.save(softwareMachine);
+        companyDao.save(dataMaesters);
+        companyDao.save(greyMatter);
+        int softwareMachineId = softwareMachine.getId();
+        int dataMaesterId = dataMaesters.getId();
+        int greyMatterId = greyMatter.getId();
+
+        assertNotEquals(0, softwareMachineId);
+        assertNotEquals(0, dataMaesterId);
+        assertNotEquals(0, greyMatterId);
+
+        try {
+            companyDao.deleteById(softwareMachineId);
+            companyDao.deleteById(dataMaesterId);
+            companyDao.deleteById(greyMatterId);
+        } catch (Exception e) {
+            // do nothing
+        }
+    }
+
+//    @Test
+//    void removeCompaniesRecords() {
+//        companyDao.deleteAll();
+//    }
 }
