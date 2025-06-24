254ebd3b083c7c95e4acb9a983a78263d3ca8897
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-06-24 21:48:44 +0200

    many to many relation in progress

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -0,0 +1,13 @@
+package com.kodilla.hibernate.manytomany.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyDaoTestSuite {
+
+    @Autowired
+    private CompanyDao companyDao;
+
+    
+}
