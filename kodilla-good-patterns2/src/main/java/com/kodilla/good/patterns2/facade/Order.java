309eb902b8b3a681d660d092e6f1715e73bf11e9
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-08-29 21:00:06 +0200

    order class created

---
+++ kodilla-good-patterns2/src/main/java/com/kodilla/good/patterns2/facade/Order.java
@@ -0,0 +1,30 @@
+package com.kodilla.good.patterns2.facade;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private ProductService productService;
+    private final List<Item> items = new ArrayList<>();
+    private final Long orderId;
+    private final Long userId;
+    private boolean isPaid;
+    private boolean isVerified;
+    private boolean isSubmitted;
+
+    public Order(Long orderId, Long userId, ProductService productService) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.productService = productService;
+    }
+
+    public BigDecimal calculateValue() {
+        BigDecimal sum = BigDecimal.ZERO;
+        for (Item item : items) {
+            sum = sum.add(productService.getPrice(item.getProductId())
+                    .multiply(new BigDecimal(item.getQty())));
+        }
+        return sum;
+    }
+}
