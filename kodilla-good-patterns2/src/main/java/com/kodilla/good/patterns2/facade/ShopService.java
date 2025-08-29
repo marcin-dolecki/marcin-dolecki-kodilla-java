ec69176fae3b5ec82712b9d5633dacd10be92adb
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-08-29 22:41:51 +0200

    shop service in progress

---
+++ kodilla-good-patterns2/src/main/java/com/kodilla/good/patterns2/facade/ShopService.java
@@ -0,0 +1,57 @@
+package com.kodilla.good.patterns2.facade;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+public class ShopService {
+    private final List<Order> orders = new ArrayList<>();
+    private final Authenticator authenticator;
+    private final ProductService productService;
+
+    @Autowired
+    public ShopService(Authenticator authenticator, ProductService productService) {
+        this.authenticator = authenticator;
+        this.productService = productService;
+    }
+
+    public Long openOrder(Long userId) {
+        if (authenticator.isAuthenticated(userId)) {
+            Long maxOrder = orders.stream()
+                    .mapToLong(o -> o.getOrderId())
+                    .max()
+                    .orElse(0);
+            orders.add(new Order(maxOrder + 1, userId, productService));
+            return maxOrder + 1;
+        } else {
+            return -1L;
+        }
+    }
+
+    public void addItem(Long orderId, Long productId, double qty) {
+        orders.stream()
+                .filter(o -> o.getOrderId().equals(orderId))
+                .forEach(o -> o.getItems().add(new Item(productId, qty)));
+    }
+
+    public boolean removeItem(Long orderId, Long productId) {
+        Iterator<Order> orderIterator = orders.stream()
+                .filter(o -> o.getOrderId().equals(orderId))
+                .iterator();
+
+        while (orderIterator.hasNext()) {
+            Order theOrder = orderIterator.next();
+            for (int n = 0; n < theOrder.getItems().size(); n++) {
+                if (theOrder.getItems().get(n).getProductId().equals(productId)) {
+                    theOrder.getItems().remove(n);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
