76b3f18098c1afcc1ae5ee7ad9c27e16a4315985
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-08-31 21:54:34 +0200

    first basic test created

---
+++ kodilla-good-patterns2/src/test/java/com/kodilla/good/patterns2/decorator/taxiportal/TaxiOrderTestSuite.java
@@ -0,0 +1,32 @@
+package com.kodilla.good.patterns2.decorator.taxiportal;
+
+import org.junit.jupiter.api.Test;
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TaxiOrderTestSuite {
+    @Test
+    void testBasicTaxiOrderGetCost() {
+        //Given
+        TaxiOrder theOrder = new BasicTaxiOrder();
+
+        //When
+        BigDecimal calculatedCost = theOrder.getCost();
+
+        //Then
+        assertEquals(new BigDecimal(5), calculatedCost);
+    }
+
+    @Test
+    void testBasicTaxiOrderGetDescription() {
+        //Given
+        TaxiOrder theOrder = new BasicTaxiOrder();
+
+        //When
+        String description = theOrder.getDescription();
+
+        //Then
+        assertEquals("Drive a course", description);
+    }
+}
