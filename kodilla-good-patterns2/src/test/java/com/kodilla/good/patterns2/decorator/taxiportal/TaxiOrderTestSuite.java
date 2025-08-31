8877e9956c81c1b810bd292d44c7d67b5150cafb
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-08-31 23:01:02 +0200

    uber test added

--- kodilla-good-patterns2/src/test/java/com/kodilla/good/patterns2/decorator/taxiportal/TaxiOrderTestSuite.java
+++ kodilla-good-patterns2/src/test/java/com/kodilla/good/patterns2/decorator/taxiportal/TaxiOrderTestSuite.java
@@ -83,4 +83,34 @@ class TaxiOrderTestSuite {
         //Then
         assertEquals("Drive a course by MyTaxi Network + child seat", description);
     }
+
+    @Test
+    void testUberWithTwoChildSeatsGetCost() {
+        //Given
+        TaxiOrder theOrder = new BasicTaxiOrder();
+        theOrder = new UberNetworkOrderDecorator(theOrder);
+        theOrder = new ChildSeatDecorator(theOrder);
+        theOrder = new ChildSeatDecorator(theOrder);
+
+        //When
+        BigDecimal theCost = theOrder.getCost();
+
+        //Then
+        assertEquals(new BigDecimal(29), theCost);
+    }
+
+    @Test
+    void testUberWithTwoChildSeatsGetDescription() {
+        //Given
+        TaxiOrder theOrder = new BasicTaxiOrder();
+        theOrder = new UberNetworkOrderDecorator(theOrder);
+        theOrder = new ChildSeatDecorator(theOrder);
+        theOrder = new ChildSeatDecorator(theOrder);
+
+        //When
+        String description = theOrder.getDescription();
+
+        //Then
+        assertEquals("Drive a course by Uber Network + child seat + child seat", description);
+    }
 }
