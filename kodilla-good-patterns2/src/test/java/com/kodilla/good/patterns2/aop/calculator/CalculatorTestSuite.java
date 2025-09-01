5a11c73ee14ed4bd824fcf3c625fada2f0e79c87
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-09-01 22:53:27 +0200

    calculator class and test completed

---
+++ kodilla-good-patterns2/src/test/java/com/kodilla/good/patterns2/aop/calculator/CalculatorTestSuite.java
@@ -0,0 +1,45 @@
+package com.kodilla.good.patterns2.aop.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class CalculatorTestSuite {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CalculatorTestSuite.class);
+
+    @Autowired
+    private Calculator calculator;
+
+    @Test
+    void testAdd() {
+        double result = calculator.add(10, 15);
+        LOGGER.info("Testing add method");
+        assertEquals(25, result, 0);
+    }
+
+    @Test
+    void testSub() {
+        double result = calculator.sub(10, 15);
+        LOGGER.info("Testing sub method");
+        assertEquals(-5, result, 0);
+    }
+
+    @Test
+    void testMul() {
+        double result = calculator.mul(10, 15);
+        LOGGER.info("Testing mul method");
+        assertEquals(150, result, 0);
+    }
+
+    @Test
+    void testDiv() {
+        double result = calculator.div(15, 5);
+        LOGGER.info("Testing div method");
+        assertEquals(3, result, 0);
+    }
+}
