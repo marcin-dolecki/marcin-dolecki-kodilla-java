43b2d48e93da59604646bfff35b824b84b416720
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-09-04 21:10:38 +0200

    find companies by name fragment test completed

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/facade/SearchFacadeTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/facade/SearchFacadeTestSuite.java
@@ -1,7 +1,39 @@
 package com.kodilla.hibernate.manytomany.facade;
 
+import com.kodilla.hibernate.manytomany.Company;
+import com.kodilla.hibernate.manytomany.dao.CompanyDao;
+import com.kodilla.hibernate.manytomany.dao.EmployeeDao;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 @SpringBootTest
-public class SearchFacadeTestSuite {
+class SearchFacadeTestSuite {
+
+    @Autowired
+    private SearchFacade searchFacade;
+
+    @Autowired
+    private CompanyDao companyDao;
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    @Test
+    void testFindCompaniesByNameFragment() {
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMasters = new Company("Data Masters");
+        companyDao.save(softwareMachine);
+        companyDao.save(dataMasters);
+
+        List<Company> result = searchFacade.findCompaniesByNameFragment("ware");
+
+        assertEquals(1, result.size());
+        assertEquals("Software Machine", result.get(0).getName());
+
+        companyDao.deleteAll();
+    }
 }
