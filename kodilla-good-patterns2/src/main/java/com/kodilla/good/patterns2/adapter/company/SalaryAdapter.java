44c7086537d362508b22d0598910416a406903d0
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-09-06 00:18:24 +0200

    adapter created

---
+++ kodilla-good-patterns2/src/main/java/com/kodilla/good/patterns2/adapter/company/SalaryAdapter.java
@@ -0,0 +1,25 @@
+package com.kodilla.good.patterns2.adapter.company;
+
+import com.kodilla.good.patterns2.adapter.company.newhrsystem.Employee;
+import com.kodilla.good.patterns2.adapter.company.oldhrsystem.SalaryCalculator;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SalaryAdapter extends SalaryAdaptee implements SalaryCalculator {
+
+    @Override
+    public double totalSalary(String[][] workers, Double[] salaries) {
+        List<Employee> employeeList = new ArrayList<>();
+        for (int n = 0; n < salaries.length; n++) {
+            employeeList.add(new Employee(
+                    workers[n][0],
+                    workers[n][1],
+                    workers[n][2],
+                    BigDecimal.valueOf(salaries[n])
+            ));
+        }
+        return calculateSalaries(employeeList).doubleValue();
+    }
+}
