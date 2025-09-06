73eefb8f594a6c5e75e07eaf484865ccb92ef5f7
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-09-06 23:06:03 +0200

    library b class and interface created

---
+++ kodilla-good-patterns2/src/main/java/com/kodilla/good/patterns2/adapter/bookclassifier/libraryb/BookSignature.java
@@ -0,0 +1,13 @@
+package com.kodilla.good.patterns2.adapter.bookclassifier.libraryb;
+
+public class BookSignature {
+    private final String signature;
+
+    public BookSignature(final String signature) {
+        this.signature = signature;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+}
