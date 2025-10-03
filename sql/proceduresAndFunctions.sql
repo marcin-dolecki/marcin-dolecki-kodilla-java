9b645b6971f08c4e02716cec5fc32a2ce6992f98
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-10-03 21:31:05 +0200

    indexes and query plans in progress

--- sql/proceduresAndFunctions.sql
+++ sql/proceduresAndFunctions.sql
@@ -480,5 +480,70 @@ DROP EVENT UPDATE_VIPS;
 SELECT * FROM BOOKS;
 
 # 28.6 Indexes and queries plans
+use kodilla_course;
 SELECT USER();
 
+CREATE TABLE PHONES (
+    PHONE_ID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    PHONENUM INT(9),
+    FIRSTNAME VARCHAR(50),
+    LASTNAME VARCHAR(50)
+);
+
+DELIMITER $$
+CREATE PROCEDURE FillTestData()
+BEGIN
+    DECLARE K INT DEFAULT 0;
+    WHILE (K < 100000) DO
+        INSERT INTO PHONES (PHONENUM,
+                            FIRSTNAME,
+                            LASTNAME)
+        VALUES (ROUND(RAND()*1000000000),
+                CONCAT('Firstname number ', K),
+                CONCAT('Lastname number ', K));
+        IF (MOD(K, 5000) = 0) THEN
+            COMMIT;
+        END IF;
+        SET K = K + 1;
+    END WHILE;
+    COMMIT;
+END $$
+DELIMITER ;
+
+CALL FillTestData();
+
+SELECT * FROM PHONES;
+
+CREATE TABLE PHONESTATS (
+    ID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    RANGE_FROM INT(11),
+    RANGE_TO INT(11),
+    QUANTITY INT(11)
+);
+
+DELIMITER $$
+CREATE PROCEDURE CalcPhoneStats()
+BEGIN
+    DECLARE K INT(11) DEFAULT 99999;
+    DECLARE QTY INT(11);
+    DELETE FROM PHONESTATS WHERE 1=1;
+    COMMIT;
+    WHILE (K < 1000000000) DO
+        SELECT COUNT(*) FROM PHONES
+            WHERE PHONENUM BETWEEN K-99999 AND K
+        INTO QTY;
+        INSERT INTO PHONESTATS (RANGE_FROM,
+                                RANGE_TO,
+                                QUANTITY)
+        VALUES (K-99999,
+                K,
+                QTY);
+        COMMIT;
+        SET K = K + 100000;
+    END WHILE;
+END $$
+DELIMITER ;
+
+CALL CalcPhoneStats();
+
+SELECT * FROM PHONESTATS;
