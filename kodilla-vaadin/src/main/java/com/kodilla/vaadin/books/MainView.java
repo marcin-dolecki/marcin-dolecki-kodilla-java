5eebea03285a724517748f27bcb9a1e9b4d04c70
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-10-30 22:33:38 +0100

    added grid with books to layout

--- kodilla-vaadin/src/main/java/com/kodilla/vaadin/books/MainView.java
+++ kodilla-vaadin/src/main/java/com/kodilla/vaadin/books/MainView.java
@@ -1,13 +1,27 @@
 package com.kodilla.vaadin.books;
 
+import com.kodilla.vaadin.books.domain.Book;
+import com.kodilla.vaadin.books.service.BookService;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route
 public class MainView extends VerticalLayout {
+
+    private BookService bookService = BookService.getInstance();
+    private Grid<Book> grid = new Grid<>(Book.class);
+
     public MainView() {
-        add(new Button("Click me", event -> Notification.show("Hello World!")));
+        grid.setColumns("title", "author", "publicationYear", "type");
+        add(grid);
+        setSizeFull();
+        refresh();
+    }
+
+    public void refresh() {
+        grid.setItems(bookService.getBooks());
     }
 }
