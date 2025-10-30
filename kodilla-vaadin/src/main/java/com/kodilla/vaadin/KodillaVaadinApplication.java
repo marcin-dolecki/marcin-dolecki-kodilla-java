c0b587000c24ed1d093cf8b0a5d7a14f45f52b11
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-10-30 22:14:20 +0100

    vaadin module created

---
+++ kodilla-vaadin/src/main/java/com/kodilla/vaadin/KodillaVaadinApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.vaadin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaVaadinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaVaadinApplication.class, args);
+    }
+
+}
---
+++ kodilla-vaadin/src/test/java/com/kodilla/vaadin/KodillaVaadinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.vaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaVaadinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
