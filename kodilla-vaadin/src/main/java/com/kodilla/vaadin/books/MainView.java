1b4a93bc0f17f1a77b5b663f023a5c5ffd03de99
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-10-30 22:15:15 +0100

    main view and book class created

---
+++ kodilla-vaadin/src/main/java/com/kodilla/vaadin/books/MainView.java
@@ -0,0 +1,13 @@
+package com.kodilla.vaadin.books;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+    public MainView() {
+        add(new Button("Click me", event -> Notification.show("Hello World!")));
+    }
+}
